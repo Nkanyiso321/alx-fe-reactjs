60aa3532868bdb140eda319d6e1e044a9c9f9097
Nkanyiso321 <nkanyisodube1234@gmail.com>
2025-11-01 00:51:45 +0200

    Add WelcomeMessage component and CSS styling

---
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -0,0 +1,14 @@
+import './WelcomeMessage.css';
+import './WelcomeMessage.css';
+
+function WelcomeMessage() {
+  return (
+    <div className="welcome-container">
+      <h1>Hello everyone, I am learning React at ALX!</h1>
+      <p>This is a simple JSX component.</p>
+      <p>I am learning about JSX!</p>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
