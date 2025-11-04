dcbf0b5b7e03dd52a12ed9179a61a7927e7087ad
Olukosi Oluwaseyi <oluwaseyiolukosi7@gmail.com>
2025-11-04 15:35:01 +0100

    Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/components/UserDetails.jsx
+++ alx-react-app-props/src/components/UserDetails.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { UserContext } from './components/UserContext.js'
+import { UserContext } from './UserContext.js'
 
 function UserDetails() {
     const userData = useContext(UserContext)
